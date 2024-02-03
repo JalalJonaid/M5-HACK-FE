652ba75bd6940aa3ffa33fee3edc98c0aff0350d
Nugget-Meister <tremainecamacho@gmail.com>
2024-02-03 15:52:01 -0500

    convert map into component for use

---
+++ src/components/common/GoogleMapWidget/GoogleMapWidget.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// Place this and Google 
+
+
+const GoogleMapWidget = () => {
+    return (
+        <div id="map">
+            
+        </div>
+    );
+}
+
+export default GoogleMapWidget;
