9afea8fbce37d3eddfba4dbbb56b6d7051e7b6fd
Nugget-Meister <tremainecamacho@gmail.com>
2024-02-04 12:44:26 -0500

    proto of get user location

---
+++ src/components/common/LocateUser/LocateUser.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+
+
+const LocateUser = () => {
+    const [location, setLocation] = useState({lat:null, long:null})
+
+    useEffect(()=> {
+        if("geolocation" in navigator){
+            // console.log(navigator.geolocation)
+            navigator.geolocation.getCurrentPosition((pos) => {
+                setLocation({
+                    lat: pos.coords.latitude,
+                    long: pos.coords.longitude
+                })
+                // console.log(pos.coords.latitude, pos.coords.longitude)
+            })
+    } 
+        
+    },[])
+
+
+    useEffect(()=>{
+        if(location.lat){
+            console.log(location)
+        }
+    }, [location])
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default LocateUser;
