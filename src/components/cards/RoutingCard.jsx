e971da386a5b33847bfb0930fd6989856d9d1684
elisaul batista <elisaulbatista@pursuit.org>
2024-02-04 13:57:08 -0500

    style the form

--- src/components/cards/RoutingCard.jsx
+++ src/components/cards/RoutingCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from 'react-bootstrap/Button';
 import RoutingForm from "../forms/RoutingForm";
-
+import './routingCard.css'
 export default function RoutingCard() {
   return (
     <div className="routingcard">
       <span className="border border-2">
-        <Card style={{ width: "20rem" }}>
+        <Card className="form" style={{ width: "20rem" }}>
           <Card.Body>
             <Card.Title>Enter Your Location</Card.Title>
             <RoutingForm></RoutingForm>
