fc0a0ef031ef4d37dc8939f5b45d9c53d3ca111d
Nugget-Meister <tremainecamacho@gmail.com>
2024-02-04 10:25:44 -0500

    init googleroutes.jsx

---
+++ src/components/common/GoogleRoute/GoogleRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const GoogleRoute = () => {
+
+
+    
+    const {DirectionsService} = await google.maps.importLibrary("routes")
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default GoogleRoute;
